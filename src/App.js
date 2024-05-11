b6c84a1383aefe156b7fa8fcc10f4f5ca27b5133
Fazalwahab12 <wahabdir567@gmail.com>
2024-05-11 03:43:40 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import "./App.css";
 import Agency from "./pages/Agency";
-import Blogs from "./pages/Blogs";
 import CommonSection from "./pages/CommonSection";
 import Contect from "./pages/Contect";
 import Footer from "./pages/Footer";
 import Header from "./pages/Header";
 import Home_slider from "./pages/Home_slider";
 import { News } from "./pages/News";
-import Our_clients from "./pages/Our_clients";
 import Portfolio from "./pages/Portfolio";
 import Services from "./pages/Services";
 import Team from "./pages/Team";
 import Trusted from "./pages/Trusted";
 import We_works from "./pages/We_works";
-
+import Reviews from "./pages/Reviews";
 function App() {
   return (
     <div>
@@ -27,7 +25,7 @@ function App() {
       <Portfolio />
       <Trusted />
       <Team />
-      <Our_clients />
+      <Reviews />
       <CommonSection />
 
       <Contect />
